181d7cfeba5ffa886405a1b821483e8c5cb53553
Nikhil Kshirsagar <nikhil.kshirsagar@lntinfotech.com>
2020-07-22 18:51:04 +0530

    Added AppScreenshot.png; Colon in chat bug fix;

--- clone-of-facebook-messenger/src/components/Message.js
+++ clone-of-facebook-messenger/src/components/Message.js
@@ -12,7 +12,7 @@ const Message = forwardRef(({loggedUsername, message}, ref) => {
             <Card className={isLoggedUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent className="">
                     <Typography color="white" variant="h5" component="h2">
-                        {!isLoggedUser && `${message.username || "Unknown user"}:`}: {message.message}   {/* NOTE: IMP*/}
+                        {!isLoggedUser && `${message.username || "Unknown user"}:`} {message.message}   {/* NOTE: IMP*/}
                     </Typography>
                 </CardContent>    
             </Card>
